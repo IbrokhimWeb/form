4014ce83ff7778c5a4431a4a801618ef11fa4507
IbrokhimWeb <ibrokhimweb@gmail.com>
2023-06-26 18:28:35 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,167 @@
+import { Box, Button, Card, TextField, TextareaAutosize } from "@mui/material";
+import React, { FormEvent, useState } from "react";
+
+const App = () => {
+  const [data, setData] = useState({
+    name: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { name, phone, message } = data;
+    let post = `--- SARBON --- %0A%0A`;
+    post += `👨‍💼 Mijoz: <b> ${name}</b> %0A`;
+    post += `📞 Raqam: <b> ${phone}</b> %0A`;
+    post += `💭 Xabar: <b> ${message}</b> %0A`;
+
+    const api = new XMLHttpRequest();
+
+    api.open(
+      "GET",
+      `https://api.telegram.org/bot${
+        import.meta.env.VITE_TOKEN
+      }/sendMessage?chat_id=${
+        import.meta.env.VITE_CHAT_ID
+      }&text=${post}&parse_mode=html`,
+      true
+    );
+    api.send();
+    setData({
+      name: "",
+      phone: "",
+      message: "",
+    });
+    alert("Siz Kiritgan Malumotlar Adminga Yuborildi");
+  };
+
+  return (
+    <>
+      <main className="w-full mt-10 flex justify-center gap-10 text-white max-[1000px]:flex-col max-[1000px]:items-center max-[1000px]:mt-0">
+        <Card className="w-[50%] h-auto box bg-[#ffffff09] rounded-2xl overflow-hidden max-[1000px]:w-full max-[1000px]:flex max-[1000px]:flex-col max-[1000px]:items-center">
+          <Box className="w-full h-full backdrop-filter backdrop-blur p-10 text-white max-[400px]:p-7">
+            <h1 className="text-[1.5em] text-center mb-7 max-[400px]:text-[1.2em]">
+              Formani to'ldiring siz bilan bog'lanishadi!
+            </h1>
+            <form
+              onSubmit={handleSubmit}
+              className="w-full h-full flex flex-col gap-5">
+              <div className="w-full flex items-center gap-5 max-[1200px]:flex-col max-[1000px]:flex-row max-[800px]:flex-col">
+                <TextField
+                  required
+                  fullWidth
+                  type="text"
+                  label="Ismingiz"
+                  value={data?.name}
+                  onChange={(e) =>
+                    setData((prev) => ({ ...prev, name: e.target.value }))
+                  }
+                  sx={{
+                    input: { color: "white" },
+                    "&:hover": {
+                      borderColor: "red",
+                    },
+                    "& label": {
+                      color: "white",
+                    },
+                    "& label.Mui-focused": {
+                      color: "white",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        borderColor: "white",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "white",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "white",
+                      },
+                    },
+                  }}
+                />
+                <TextField
+                  required
+                  fullWidth
+                  type="tel"
+                  label="Telefon raqamingiz"
+                  value={data?.phone}
+                  onChange={(e) =>
+                    setData((prev) => ({ ...prev, phone: e.target.value }))
+                  }
+                  sx={{
+                    input: { color: "white" },
+                    "&:hover": {
+                      borderColor: "red",
+                    },
+                    "& label": {
+                      color: "white",
+                    },
+                    "& label.Mui-focused": {
+                      color: "white",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        borderColor: "white",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "white",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "white",
+                      },
+                    },
+                  }}
+                />
+              </div>
+              <TextareaAutosize
+                required
+                minRows={3}
+                value={data?.message}
+                placeholder="Xabaringiz*"
+                className="w-full p-5 rounded-md bg-transparent border-2"
+                onChange={(e) =>
+                  setData((prev) => ({ ...prev, message: e.target.value }))
+                }
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                className="bg-green-600">
+                Yuborish
+              </Button>
+            </form>
+          </Box>
+        </Card>
+      </main>
+      <footer className="relative w-full h-[50vh] flex flex-col items-center justify-center gap-3 py-10 text-white">
+        <h1 className="text-[1.1em] font-bold">
+          Ish vaqti:{" "}
+          <span className="text-[.9em] font-light">
+            Dush - Jum, 10:00 - 18:30
+          </span>
+        </h1>
+        <h1 className="text-[1.1em] font-bold">
+          Bizning raqam:{" "}
+          <span className="text-[.9em] font-light">
+            {import.meta.env.VITE_PHONE}
+          </span>
+        </h1>
+        <h1 className="text-[1.1em] font-bold">
+          Email: <span className="text-[.9em] font-light">info@sarbon.com</span>
+        </h1>
+        <h1 className="text-[1.1em] font-bold text-center">
+          Manzil: <br />
+          <span className="text-[.9em] font-light">
+            Sarbon Agency, 100100, Uzbekistan, Tashkent city, Labzak-2A street,
+            1
+          </span>
+        </h1>
+        <p>© Barcha huquqlar himoyalangan 2023.</p>
+      </footer>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  important: "#root",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
